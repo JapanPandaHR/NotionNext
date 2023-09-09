f5783360b444d72be295d96effed8cebded791ec
JapanPandaHR <144541027+JapanPandaHR@users.noreply.github.com>
2023-09-10 02:15:26 +0900

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -13,7 +13,7 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
-  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || '我们旨在帮助更过的朋友自由、快速、合理地选择适合自己的赴日道路。从而使得生活变得更美好！',
+  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || '自由、快速、合理地选择赴日途径。',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
