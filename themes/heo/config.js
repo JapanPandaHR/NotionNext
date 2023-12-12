a168a0d19c0cedac6e265005d6a6e0c36e37aec9
JapanPandaHR <144541027+JapanPandaHR@users.noreply.github.com>
2023-12-12 10:19:18 +0900

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -18,8 +18,8 @@ const CONFIG = {
   HERO_TITLE_LINK: 'https://www.pandahr.jp/tag/%E8%8B%B1%E9%9B%84%E6%A6%9C',
 
   // 英雄区显示三个置顶分类
-  HERO_CATEGORY_1: { title: '赴日就职案件', url: '/tag/赴日就职' },
-  HERO_CATEGORY_2: { title: '在日就职案件', url: '/tag/在日就职' },
+  HERO_CATEGORY_1: { title: '服务流程', url: '/service' },
+  HERO_CATEGORY_2: { title: '关于我们', url: '/company' },
   HERO_CATEGORY_3: { title: '新手必看', url: '/tag/新手必看' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
